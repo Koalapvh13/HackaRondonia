aff96c481ff480d5b4a0b8cb205b89d133162b80
Matheus Dias Vieira <matheuspvh13@gmail.com>
2018-08-03 23:18:49 -0400

    tela de cadastro I.I

---
+++ app/src/main/java/com/koala/gps/Cadastro2.java
@@ -0,0 +1,13 @@
+package com.koala.gps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cadastro2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro2);
+    }
+}
